ce645c44ee15da1ba5659e7f913e7ac2b4f7ba45
tim_iles <tim@timiles.com>
2014-06-17 13:46:41 +0100

    init(); testCounterMove();

--- public/js/backgammon.js
+++ public/js/backgammon.js
@@ -4,43 +4,80 @@ var Backgammon = {
     Players: { Red : 0, Black: 1 },
     Board: function(boardElementId) {
 
-        var $board = $('#' + boardElementId);
-        $board.addClass('board')
-            .append($('<div id="13" class="pip top-pip red-pip">'))
-            .append($('<div id="14" class="pip top-pip black-pip">'))
-            .append($('<div id="15" class="pip top-pip red-pip">'))
-            .append($('<div id="16" class="pip top-pip black-pip">'))
-            .append($('<div id="17" class="pip top-pip red-pip">'))
-            .append($('<div id="18" class="pip top-pip black-pip">'))
-            .append($('<div id="black-bar" class="pip bar">'))
-            .append($('<div id="19" class="pip top-pip red-pip">'))
-            .append($('<div id="20" class="pip top-pip black-pip">'))
-            .append($('<div id="21" class="pip top-pip red-pip">'))
-            .append($('<div id="22" class="pip top-pip black-pip">'))
-            .append($('<div id="23" class="pip top-pip red-pip">'))
-            .append($('<div id="24" class="pip top-pip black-pip">'))
-            .append($('<div id="black-home" class="pip home">'))
-            .append($('<br class="clear">'))
-            .append($('<div id="12" class="pip bottom-pip black-pip">'))
-            .append($('<div id="11" class="pip bottom-pip red-pip">'))
-            .append($('<div id="10" class="pip bottom-pip black-pip">'))
-            .append($('<div id="9" class="pip bottom-pip red-pip">'))
-            .append($('<div id="8" class="pip bottom-pip black-pip">'))
-            .append($('<div id="7" class="pip bottom-pip red-pip">'))
-            .append($('<div id="red-bar" class="pip bar">'))
-            .append($('<div id="6" class="pip bottom-pip black-pip">'))
-            .append($('<div id="5" class="pip bottom-pip red-pip">'))
-            .append($('<div id="4" class="pip bottom-pip black-pip">'))
-            .append($('<div id="3" class="pip bottom-pip red-pip">'))
-            .append($('<div id="2" class="pip bottom-pip black-pip">'))
-            .append($('<div id="1" class="pip bottom-pip red-pip">'))
-            .append($('<div id="red-home" class="pip home">'))
-            .append($('<br class="clear">'));
-
         // layout: 0 is home
-        var layout = new Array(25);
-        for (var i = 0; i < 25; i++) {
-            layout[i] = [0, 0];
+        var layout;
+
+        this.init = function() {
+            layout = new Array(25);
+
+            var $board = $('#' + boardElementId);
+            $board.empty();
+            $board.addClass('board')
+                .append($('<div id="13" class="pip top-pip red-pip">'))
+                .append($('<div id="14" class="pip top-pip black-pip">'))
+                .append($('<div id="15" class="pip top-pip red-pip">'))
+                .append($('<div id="16" class="pip top-pip black-pip">'))
+                .append($('<div id="17" class="pip top-pip red-pip">'))
+                .append($('<div id="18" class="pip top-pip black-pip">'))
+                .append($('<div id="black-bar" class="pip bar">'))
+                .append($('<div id="19" class="pip top-pip red-pip">'))
+                .append($('<div id="20" class="pip top-pip black-pip">'))
+                .append($('<div id="21" class="pip top-pip red-pip">'))
+                .append($('<div id="22" class="pip top-pip black-pip">'))
+                .append($('<div id="23" class="pip top-pip red-pip">'))
+                .append($('<div id="24" class="pip top-pip black-pip">'))
+                .append($('<div id="black-home" class="pip home">'))
+                .append($('<br class="clear">'))
+                .append($('<div id="12" class="pip bottom-pip black-pip">'))
+                .append($('<div id="11" class="pip bottom-pip red-pip">'))
+                .append($('<div id="10" class="pip bottom-pip black-pip">'))
+                .append($('<div id="9" class="pip bottom-pip red-pip">'))
+                .append($('<div id="8" class="pip bottom-pip black-pip">'))
+                .append($('<div id="7" class="pip bottom-pip red-pip">'))
+                .append($('<div id="red-bar" class="pip bar">'))
+                .append($('<div id="6" class="pip bottom-pip black-pip">'))
+                .append($('<div id="5" class="pip bottom-pip red-pip">'))
+                .append($('<div id="4" class="pip bottom-pip black-pip">'))
+                .append($('<div id="3" class="pip bottom-pip red-pip">'))
+                .append($('<div id="2" class="pip bottom-pip black-pip">'))
+                .append($('<div id="1" class="pip bottom-pip red-pip">'))
+                .append($('<div id="red-home" class="pip home">'))
+                .append($('<br class="clear">'));
+
+            for (var i = 0; i < 25; i++) {
+                layout[i] = [0, 0];
+            }
+
+            this.addCounterToPip(24, Backgammon.Players.Red);
+            this.addCounterToPip(24, Backgammon.Players.Red);
+            this.addCounterToPip(1, Backgammon.Players.Black);
+            this.addCounterToPip(1, Backgammon.Players.Black);
+            this.addCounterToPip(6, Backgammon.Players.Red);
+            this.addCounterToPip(6, Backgammon.Players.Red);
+            this.addCounterToPip(6, Backgammon.Players.Red);
+            this.addCounterToPip(6, Backgammon.Players.Red);
+            this.addCounterToPip(6, Backgammon.Players.Red);
+            this.addCounterToPip(19, Backgammon.Players.Black);
+            this.addCounterToPip(19, Backgammon.Players.Black);
+            this.addCounterToPip(19, Backgammon.Players.Black);
+            this.addCounterToPip(19, Backgammon.Players.Black);
+            this.addCounterToPip(19, Backgammon.Players.Black);
+            this.addCounterToPip(8, Backgammon.Players.Red);
+            this.addCounterToPip(8, Backgammon.Players.Red);
+            this.addCounterToPip(8, Backgammon.Players.Red);
+            this.addCounterToPip(17, Backgammon.Players.Black);
+            this.addCounterToPip(17, Backgammon.Players.Black);
+            this.addCounterToPip(17, Backgammon.Players.Black);
+            this.addCounterToPip(13, Backgammon.Players.Red);
+            this.addCounterToPip(13, Backgammon.Players.Red);
+            this.addCounterToPip(13, Backgammon.Players.Red);
+            this.addCounterToPip(13, Backgammon.Players.Red);
+            this.addCounterToPip(13, Backgammon.Players.Red);
+            this.addCounterToPip(12, Backgammon.Players.Black);
+            this.addCounterToPip(12, Backgammon.Players.Black);
+            this.addCounterToPip(12, Backgammon.Players.Black);
+            this.addCounterToPip(12, Backgammon.Players.Black);
+            this.addCounterToPip(12, Backgammon.Players.Black);
         }
 
         this.addCounterToPip = function(pip, player) {
@@ -54,51 +91,40 @@ var Backgammon = {
 
             if (pip == 0) {
                 // special case for home pips:
-                $('DIV#' + playerName + '-home').append($('<DIV class="counter">').addClass(playerName).text(totalCounters));
+                $('div#' + playerName + '-home').append($('<div class="counter">').addClass(playerName).text(totalCounters));
             }
             else {
-                var $pipDiv = $('DIV#' + pip);
+                var $pipDiv = $('div#' + pip);
                 if (totalCounters > 5) {
-                    $pipDiv.append($('<DIV class="counter">').addClass(playerName).text(totalCounters));
+                    $pipDiv.append($('<div class="counter">').addClass(playerName).text(totalCounters));
                 } else {
-                    $pipDiv.append($('<DIV class="counter">').addClass(playerName));
+                    $pipDiv.append($('<div class="counter">').addClass(playerName));
                 }
             }
         }
 
-        this.addCounterToPip(24, Backgammon.Players.Red);
-        this.addCounterToPip(24, Backgammon.Players.Red);
-        this.addCounterToPip(1, Backgammon.Players.Black);
-        this.addCounterToPip(1, Backgammon.Players.Black);
-        this.addCounterToPip(6, Backgammon.Players.Red);
-        this.addCounterToPip(6, Backgammon.Players.Red);
-        this.addCounterToPip(6, Backgammon.Players.Red);
-        this.addCounterToPip(6, Backgammon.Players.Red);
-        this.addCounterToPip(6, Backgammon.Players.Red);
-        this.addCounterToPip(19, Backgammon.Players.Black);
-        this.addCounterToPip(19, Backgammon.Players.Black);
-        this.addCounterToPip(19, Backgammon.Players.Black);
-        this.addCounterToPip(19, Backgammon.Players.Black);
-        this.addCounterToPip(19, Backgammon.Players.Black);
-        this.addCounterToPip(8, Backgammon.Players.Red);
-        this.addCounterToPip(8, Backgammon.Players.Red);
-        this.addCounterToPip(8, Backgammon.Players.Red);
-        this.addCounterToPip(17, Backgammon.Players.Black);
-        this.addCounterToPip(17, Backgammon.Players.Black);
-        this.addCounterToPip(17, Backgammon.Players.Black);
-        this.addCounterToPip(13, Backgammon.Players.Red);
-        this.addCounterToPip(13, Backgammon.Players.Red);
-        this.addCounterToPip(13, Backgammon.Players.Red);
-        this.addCounterToPip(13, Backgammon.Players.Red);
-        this.addCounterToPip(13, Backgammon.Players.Red);
-        this.addCounterToPip(12, Backgammon.Players.Black);
-        this.addCounterToPip(12, Backgammon.Players.Black);
-        this.addCounterToPip(12, Backgammon.Players.Black);
-        this.addCounterToPip(12, Backgammon.Players.Black);
-        this.addCounterToPip(12, Backgammon.Players.Black);
-
-        this.getPip = function(pip) {
-            return layout[pip];
+        this.testMoveCounter = function(pipNumber, moves) {
+            var pip = this.getPip(pipNumber);
+            // case: there is no counter to move: fail
+            if (pip[0] == 0 && pip[1] == 0) {
+                return false;
+            }
+            var me = pip[0] > 0 ? 0 : 1;
+            var opp = (me == 0) ? 1 : 0;
+            var dir = (me == 0) ? -1 : 1;
+            // case: there is a counter, but opponent blocks the end pip
+            if (this.getPip(pipNumber + (moves*dir))[opp] >= 2) {
+                return false;
+            }
+            // todo: more checks, eg player can bear off etc
+            return true;
         }
+
+        this.getPip = function(pipNumber) {
+            return layout[pipNumber];
+        }
+
+
+        this.init();
     }
 };
--- tests/specs.js
+++ tests/specs.js
@@ -12,4 +12,34 @@ describe('Backgammon', function () {
         expect(board.getPip(24)[Backgammon.Players.Red]).toBe(2);
         expect(board.getPip(24)[Backgammon.Players.Black]).toBe(0);
     });
+    
+    it('should detect illegal and legal moves for Red', function () {
+
+        // test for Red
+        expect(board.getPip(24)[Backgammon.Players.Red]).toBe(2);
+
+        var isLegal1 = board.testMoveCounter(24, 6);
+        expect(isLegal1).toBe(true);
+        var isLegal2 = board.testMoveCounter(24, 5);
+        expect(isLegal2).toBe(false); // end pip is blocked
+
+        var isLegal3 = board.testMoveCounter(23, 1);
+        expect(isLegal3).toBe(false); // start pip contains no counter
+
+    });
+
+    it('should detect illegal and legal moves for Black', function () {
+
+        // test for Black
+        expect(board.getPip(1)[Backgammon.Players.Black]).toBe(2);
+        var isLegal1 = board.testMoveCounter(1, 6);
+        expect(isLegal1).toBe(true);
+        var isLegal2 = board.testMoveCounter(1, 5);
+        expect(isLegal2).toBe(false); // end pip is blocked
+
+        var isLegal3 = board.testMoveCounter(2, 1);
+        expect(isLegal3).toBe(false); // start pip contains no counter
+
+    });
+
 });
