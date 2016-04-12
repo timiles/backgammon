3890450e444090993171daf00d5b6c8fb3818e70
Tim Iles <tim_iles@live.com>
2016-04-12 21:17:40 +0100

    black/red as enum

--- src/Board.ts
+++ src/Board.ts
@@ -1,3 +1,5 @@
+enum Player { BLACK, RED }
+
 class Board {
     
     boardDiv: HTMLElement;
@@ -8,53 +10,53 @@ class Board {
         // TODO: check board element is empty
         this.boardDiv.className = 'board';
         
-        this.boardDiv.appendChild(Board.createPip(13, 'top', 'red'));
-        this.boardDiv.appendChild(Board.createPip(14, 'top', 'black'));
-        this.boardDiv.appendChild(Board.createPip(15, 'top', 'red'));
-        this.boardDiv.appendChild(Board.createPip(16, 'top', 'black'));
-        this.boardDiv.appendChild(Board.createPip(17, 'top', 'red'));
-        this.boardDiv.appendChild(Board.createPip(18, 'top', 'black'));
-        this.boardDiv.appendChild(Board.createBar('black'));
-        this.boardDiv.appendChild(Board.createPip(19, 'top', 'red'));
-        this.boardDiv.appendChild(Board.createPip(20, 'top', 'black'));
-        this.boardDiv.appendChild(Board.createPip(21, 'top', 'red'));
-        this.boardDiv.appendChild(Board.createPip(22, 'top', 'black'));
-        this.boardDiv.appendChild(Board.createPip(23, 'top', 'red'));
-        this.boardDiv.appendChild(Board.createPip(24, 'top', 'black'));
-        this.boardDiv.appendChild(Board.createHome('black'));
+        this.boardDiv.appendChild(Board.createPip(13, 'top', Player.RED));
+        this.boardDiv.appendChild(Board.createPip(14, 'top', Player.BLACK));
+        this.boardDiv.appendChild(Board.createPip(15, 'top', Player.RED));
+        this.boardDiv.appendChild(Board.createPip(16, 'top', Player.BLACK));
+        this.boardDiv.appendChild(Board.createPip(17, 'top', Player.RED));
+        this.boardDiv.appendChild(Board.createPip(18, 'top', Player.BLACK));
+        this.boardDiv.appendChild(Board.createBar(Player.BLACK));
+        this.boardDiv.appendChild(Board.createPip(19, 'top', Player.RED));
+        this.boardDiv.appendChild(Board.createPip(20, 'top', Player.BLACK));
+        this.boardDiv.appendChild(Board.createPip(21, 'top', Player.RED));
+        this.boardDiv.appendChild(Board.createPip(22, 'top', Player.BLACK));
+        this.boardDiv.appendChild(Board.createPip(23, 'top', Player.RED));
+        this.boardDiv.appendChild(Board.createPip(24, 'top', Player.BLACK));
+        this.boardDiv.appendChild(Board.createHome(Player.BLACK));
         this.boardDiv.appendChild(Board.createClearBreak());
-        this.boardDiv.appendChild(Board.createPip(12, 'bottom', 'black'));
-        this.boardDiv.appendChild(Board.createPip(11, 'bottom', 'red'));
-        this.boardDiv.appendChild(Board.createPip(10, 'bottom', 'black'));
-        this.boardDiv.appendChild(Board.createPip(9, 'bottom', 'red'));
-        this.boardDiv.appendChild(Board.createPip(8, 'bottom', 'black'));
-        this.boardDiv.appendChild(Board.createPip(7, 'bottom', 'red'));
-        this.boardDiv.appendChild(Board.createBar('red'));
-        this.boardDiv.appendChild(Board.createPip(6, 'bottom', 'black'));
-        this.boardDiv.appendChild(Board.createPip(5, 'bottom', 'red'));
-        this.boardDiv.appendChild(Board.createPip(4, 'bottom', 'black'));
-        this.boardDiv.appendChild(Board.createPip(3, 'bottom', 'red'));
-        this.boardDiv.appendChild(Board.createPip(2, 'bottom', 'black'));
-        this.boardDiv.appendChild(Board.createPip(1, 'bottom', 'red'));
-        this.boardDiv.appendChild(Board.createHome('red'));
+        this.boardDiv.appendChild(Board.createPip(12, 'bottom', Player.BLACK));
+        this.boardDiv.appendChild(Board.createPip(11, 'bottom', Player.RED));
+        this.boardDiv.appendChild(Board.createPip(10, 'bottom', Player.BLACK));
+        this.boardDiv.appendChild(Board.createPip(9, 'bottom', Player.RED));
+        this.boardDiv.appendChild(Board.createPip(8, 'bottom', Player.BLACK));
+        this.boardDiv.appendChild(Board.createPip(7, 'bottom', Player.RED));
+        this.boardDiv.appendChild(Board.createBar(Player.RED));
+        this.boardDiv.appendChild(Board.createPip(6, 'bottom', Player.BLACK));
+        this.boardDiv.appendChild(Board.createPip(5, 'bottom', Player.RED));
+        this.boardDiv.appendChild(Board.createPip(4, 'bottom', Player.BLACK));
+        this.boardDiv.appendChild(Board.createPip(3, 'bottom', Player.RED));
+        this.boardDiv.appendChild(Board.createPip(2, 'bottom', Player.BLACK));
+        this.boardDiv.appendChild(Board.createPip(1, 'bottom', Player.RED));
+        this.boardDiv.appendChild(Board.createHome(Player.RED));
         this.boardDiv.appendChild(Board.createClearBreak());
     }
     
-    private static createPip(pipNumber: Number, side: string, colour: string) {
+    private static createPip(pipNumber: Number, side: string, player: Player) {
         var pip = document.createElement('div');
         pip.id = pipNumber.toString();
-        pip.className = `pip ${side}-pip ${colour}-pip`;
+        pip.className = `pip ${side}-pip ${Player[player].toLocaleString().toLowerCase()}-pip`;
         return pip;
     }
-    private static createBar(colour: string) {
+    private static createBar(player: Player) {
         var bar = document.createElement('div');
-        bar.id = colour + '-bar';
+        bar.id = Player[player].toLocaleString().toLowerCase() + '-bar';
         bar.className = 'pip bar';
         return bar;
     }
-    private static createHome(colour: string) {
+    private static createHome(player: Player) {
         var bar = document.createElement('div');
-        bar.id = colour + '-home';
+        bar.id = Player[player].toLocaleString().toLowerCase() + '-home';
         bar.className = 'pip home';
         return bar;
     }
