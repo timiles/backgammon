f72cd0d42ce8ed9b18d67a34ab15a1b7764e9e1b
Tim Iles <tim_iles@live.com>
2016-05-09 22:10:23 +0100

    test sources with 5 5 dice

--- tests/ui.js
+++ tests/ui.js
@@ -1,17 +1,36 @@
 'use strict';
 
-var board;
+var game;
+var fakeDiceRollGenerator;
 
 describe('UI', function () {
 
     beforeEach(function () {
         
         let ui = new GameUI('backgammon');
-        board = new Board(ui.boardUI);
-        let dice = new Dice(new DiceRollGenerator(), ui.blackDiceUI, ui.redDiceUI);
+        let board = new Board(ui.boardUI);
+        
+        let FakeDiceRollGenerator = (function () {
+            var diceRolls = [5, 5];
+            var sequenceIndex = 0;
+            function FakeDiceRollGenerator() {
+            }
+            FakeDiceRollGenerator.prototype.generateDiceRoll = function () {
+                var nextRoll = diceRolls[sequenceIndex];
+                sequenceIndex = ++sequenceIndex % diceRolls.length;
+                return diceRolls[sequenceIndex];
+            };
+            FakeDiceRollGenerator.prototype.setDiceRolls = function (d) {
+                diceRolls = d;
+            };
+            return FakeDiceRollGenerator;
+        })();
+        fakeDiceRollGenerator = new FakeDiceRollGenerator();
+
+        let dice = new Dice(fakeDiceRollGenerator, ui.blackDiceUI, ui.redDiceUI);
         let statusLogger = new StatusLogger(ui.statusUI);
 
-        new Game(ui, board, dice, statusLogger);
+        game = new Game(ui, board, dice, statusLogger);
     });
 
     it('should initialise a standard starting board', function () {
@@ -23,6 +42,39 @@ describe('UI', function () {
         expect($('#backgammon_point17').children('.black').length).toBe(3);
         expect($('#backgammon_point19').children('.black').length).toBe(5);
         expect($('#backgammon_point24').children('.red').length).toBe(2);
+    });
+
+    it('should highlight valid sources', function () {
+        // black: true where move is possible
+        expect($('#backgammon_point1').hasClass('state-valid-source')).toBe(false);
+        expect($('#backgammon_point12').hasClass('state-valid-source')).toBe(true);
+        expect($('#backgammon_point17').hasClass('state-valid-source')).toBe(true);
+        expect($('#backgammon_point19').hasClass('state-valid-source')).toBe(false);
+        
+        // red: none true
+        expect($('#backgammon_point6').hasClass('state-valid-source')).toBe(false);
+        expect($('#backgammon_point8').hasClass('state-valid-source')).toBe(false);
+        expect($('#backgammon_point13').hasClass('state-valid-source')).toBe(false);
+        expect($('#backgammon_point24').hasClass('state-valid-source')).toBe(false);
+
+        // rest of the board: none true
+        expect($('#backgammon_point2').hasClass('state-valid-source')).toBe(false);
+        expect($('#backgammon_point3').hasClass('state-valid-source')).toBe(false);
+        expect($('#backgammon_point4').hasClass('state-valid-source')).toBe(false);
+        expect($('#backgammon_point5').hasClass('state-valid-source')).toBe(false);
+        expect($('#backgammon_point7').hasClass('state-valid-source')).toBe(false);
+        expect($('#backgammon_point9').hasClass('state-valid-source')).toBe(false);
+        expect($('#backgammon_point10').hasClass('state-valid-source')).toBe(false);
+        expect($('#backgammon_point11').hasClass('state-valid-source')).toBe(false);
+        expect($('#backgammon_point14').hasClass('state-valid-source')).toBe(false);
+        expect($('#backgammon_point15').hasClass('state-valid-source')).toBe(false);
+        expect($('#backgammon_point16').hasClass('state-valid-source')).toBe(false);
+        expect($('#backgammon_point18').hasClass('state-valid-source')).toBe(false);
+        expect($('#backgammon_point20').hasClass('state-valid-source')).toBe(false);
+        expect($('#backgammon_point21').hasClass('state-valid-source')).toBe(false);
+        expect($('#backgammon_point22').hasClass('state-valid-source')).toBe(false);
+        expect($('#backgammon_point23').hasClass('state-valid-source')).toBe(false);
+        
     });
 
 });
